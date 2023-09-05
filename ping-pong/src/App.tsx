a8f38bfe7d682332b6e5c665dfaa094dafba4fd8
Adnane Hamdy <ahamdy@c4r10p10.1337.ma>
2023-09-05 17:24:35 +0100

    done

---
+++ ping-pong/src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+import Sketch from './components/Sketch';
+
+function App()
+{
+  return (<div className='App'>
+    <Sketch/>
+  </div>);
+}
+
+
+export default App;
